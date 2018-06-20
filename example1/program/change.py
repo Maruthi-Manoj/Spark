b4891a4b4f0257a382fea69f17bb5d01ca1953b3
Maruthi-Manoj <maruthi.manoj.edu@gmail.com>
2018-06-20 18:42:07 +0000

    Changed files hierarchy and files alignment

--- example1/program/change.py
+++ example1/program/change.py
@@ -8,28 +8,42 @@ def filename():
 	weekoftheyear = str(now.isocalendar()[1])
 	currentdate = now.strftime("%Y%m%d")
 	temp = temp+weekoftheyear+"_"+currentdate+".csv"
-	print temp
+	return temp
+
 if __name__ == "__main__":
-	session = SparkSession.builder.appName("change").master("local[*]").getOrCreate()
-	dataframereader = session.read
+	
+	spark = SparkSession \
+		.builder \
+		.appName("change") \
+		.master("local[*]") \
+		.getOrCreate()
 	
 	#Reading data from csv as a dataframe
         
-	hierarchical_data = dataframereader.option("header","true").option("inferSchema", value = True).csv("/home/maruthimanoj_edu/spark/spark-usecases/Usecase_1/data/sample_data/hierarchical_data.csv")	   
-	hierarchical_data_new = dataframereader.option("header","true").option("inferSchema", value = True).csv("/home/maruthimanoj_edu/spark/spark-usecases/Usecase_1/data/sample_data/hierarchical_data_new.csv")
+	hierarchical_data = spark.read \
+				.option("header","true") \
+				.option("inferSchema", value = True) \
+				.csv("data/hierarchical_data.csv")	   
+	hierarchical_data_new = spark.read \
+					.option("header","true") \
+					.option("inferSchema", value = True) \
+					.csv("data/hierarchical_data_new.csv")
 	
 	# Joining two dataframes  to get change in brandnames	
 	brandchanges = hierarchical_data \
 		.join(hierarchical_data_new, (hierarchical_data.Product_key == hierarchical_data_new.Product_key) & (hierarchical_data.brand != hierarchical_data_new.brand) , 'inner') \
-		.select(hierarchical_data.brand_key, hierarchical_data.brand, hierarchical_data_new.brand).distinct()
+		.select(hierarchical_data.brand_key, hierarchical_data.brand, hierarchical_data_new.brand) \
+		.distinct()
 	# Joining two dataframes to get change in Category
 	categorychanges =  hierarchical_data \
-                .join(hierarchical_data_new, (hierarchical_data.Product_key == hierarchical_data_new.Product_key) & (hierarchical_data.Category != hierarchical_data_new.Category) , 'inner') \
-                .select(hierarchical_data.brand_key, hierarchical_data.Category, hierarchical_data_new.Category).distinct()
+               .join(hierarchical_data_new, (hierarchical_data.Product_key == hierarchical_data_new.Product_key) & (hierarchical_data.Category != hierarchical_data_new.Category) , 'inner') \
+               .select(hierarchical_data.brand_key, hierarchical_data.Category, hierarchical_data_new.Category) \
+	       .distinct()
 	# Joining two dataframs to get change in Vertical
 	verticalchanges =  hierarchical_data \
-                .join(hierarchical_data_new, (hierarchical_data.Product_key == hierarchical_data_new.Product_key) & (hierarchical_data.Vertical != hierarchical_data_new.Vertical) , 'inner') \
-                .select(hierarchical_data.brand_key, hierarchical_data.Vertical, hierarchical_data_new.Vertical).distinct()
+               .join(hierarchical_data_new, (hierarchical_data.Product_key == hierarchical_data_new.Product_key) & (hierarchical_data.Vertical != hierarchical_data_new.Vertical) , 'inner') \
+               .select(hierarchical_data.brand_key, hierarchical_data.Vertical, hierarchical_data_new.Vertical) \
+	       .distinct()
 	a=brandchanges.collect()
 	b=categorychanges.collect()
 	c=verticalchanges.collect()
@@ -37,4 +51,3 @@ if __name__ == "__main__":
 	print(b)
 	print(c)
 	#sumvalue.coalesce(1).write.option("header","true").csv("/home/maruthimanoj_edu/spark/spark-usecases/Usecase_1/output/sum")
-	filename()
--- example1/program/join_two_csv_and_aggregate.py
+++ example1/program/join_two_csv_and_aggregate.py
@@ -1,15 +1,41 @@
 from pyspark.sql import SparkSession
+import datetime
+from datetime import date
+#function to get filename as change_capture_weekoftheyear_yyyymmdd.csv
+def filename():
+        temp = "join_two_csv_and_aggregate_"
+        now = datetime.datetime.now()
+        weekoftheyear = str(now.isocalendar()[1])
+        currentdate = now.strftime("%Y%m%d")
+        temp = temp+weekoftheyear+"_"+currentdate+".csv"
+        return temp
 
 if __name__ == "__main__":
-	session = SparkSession.builder.appName(" join two csv and aggregate").master("local[*]").getOrCreate()
-	dataframereader = session.read
-        hierarchical_data = dataframereader.option("header","true").option("inferSchema", value = True).csv("/home/maruthimanoj_edu/spark/spark-usecases/Usecase_1/data/sample_data/hierarchical_data.csv")	   
-	product_sales_data = dataframereader.option("header","true").option("inferSchema", value = True).csv("/home/maruthimanoj_edu/spark/spark-usecases/Usecase_1/data/sample_data/product_sales_data.csv")
+	
+	spark = SparkSession \
+		  .builder \
+		  .appName("join two csv and aggregate") \
+		  .master("local[*]") \
+                  .getOrCreate()
+        hierarchical_data = spark.read \
+				.option("header","true") \
+				.option("inferSchema", value = True) \
+				.csv("data/hierarchical_data.csv")	   
+	product_sales_data = spark.read \
+				.option("header","true") \
+				.option("inferSchema", value = True) \
+				.csv("data/product_sales_data.csv")
 	hierarchical_data.printSchema()
 	product_sales_data.printSchema()
-	joinandselect = hierarchical_data.join(product_sales_data, hierarchical_data.Product_key == product_sales_data.product_key,'inner').select(hierarchical_data.brand, hierarchical_data.Product_key, product_sales_data.sales,)
+	joinandselect = hierarchical_data \
+	     .join(product_sales_data, hierarchical_data.Product_key == product_sales_data.product_key,'inner') \
+	     .select(hierarchical_data.brand, hierarchical_data.Product_key, product_sales_data.sales)
 	print(joinandselect)
 	sumvalue = joinandselect.groupBy('brand').sum('sales')
-	print(sumvalue.collect())
-	sumvalue.coalesce(1).write.option("header","true").csv("/home/maruthimanoj_edu/spark/spark-usecases/Usecase_1/output/sum")
+	temp = filename()
+	sumvalue \
+	.coalesce(1) \
+	.write \
+	.option("header","true") \
+	.csv("output/"+temp)
 
